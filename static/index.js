78ebe037172a643604a84dc8f3e6e732a3371156
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-02-07 18:22:14 -0200

    timeout and string literals

--- static/index.js
+++ static/index.js
@@ -13,9 +13,15 @@ var app = new Vue({
       });
     },
     change(state, ns, name) {
-      axios({ method: "PUT", "url": "/api/deployments/"+ns+"/"+name+"/"+state }).then(result => {
-        this.msg = name + ' (' + ns + '): going ' + state;
+      axios({
+        method: "PUT",
+        url: `/api/deployments/${ns}/${name}/${state}`
+      }).then(result => {
+        this.msg = `${name} (${ns}): going ${state}`;
         this.get();
+        window.setTimeout(() => {
+          this.msg = ''
+        }, 5000);
       }, error => {
         console.error(error);
       });
